Tested xcor(). Fixed masking status report.

tayph/ccf.py
@@ -70,17 +70,17 @@ def xcor(list_of_wls,list_of_orders,wlm,fxm,drv,RVrange,plot=False,list_of_error
     if len(list_of_wls) != len(list_of_orders):
         raise ValueError(f'In xcor(): List of wls and list of orders have different length ({len(list_of_wls)} & {len(list_of_orders)}).')
 
-    ut.dimtest(wlm,[len(fxm)],'wlm in ccf.xcor()')
-    ut.typetest(wlm,np.ndarray,'wlm in ccf.xcor')
-    ut.typetest(fxm,np.ndarray,'fxm in ccf.xcor')
-    ut.typetest(drv,[int,float],'drv in ccf.xcor')
-    ut.typetest(RVrange,float,'RVrange in ccf.xcor()',)
-    ut.postest(RVrange,'RVrange in ccf.xcor()')
-    ut.postest(drv,'drv in ccf.xcor()')
-    ut.nantest(wlm,'fxm in ccf.xcor()')
-    ut.nantest(fxm,'fxm in ccf.xcor()')
-    ut.nantest(drv,'drv in ccf.xcor()')
-    ut.nantest(RVrange,'RVrange in ccf.xcor()')
+    dimtest(wlm,[len(fxm)],'wlm in ccf.xcor()')
+    typetest(wlm,np.ndarray,'wlm in ccf.xcor')
+    typetest(fxm,np.ndarray,'fxm in ccf.xcor')
+    typetest(drv,[int,float],'drv in ccf.xcor')
+    typetest(RVrange,float,'RVrange in ccf.xcor()',)
+    postest(RVrange,'RVrange in ccf.xcor()')
+    postest(drv,'drv in ccf.xcor()')
+    nantest(wlm,'fxm in ccf.xcor()')
+    nantest(fxm,'fxm in ccf.xcor()')
+    nantest(drv,'drv in ccf.xcor()')
+    nantest(RVrange,'RVrange in ccf.xcor()')
     drv = float(drv)
     N=len(list_of_wls)#Number of orders.
 
@@ -88,7 +88,7 @@ def xcor(list_of_wls,list_of_orders,wlm,fxm,drv,RVrange,plot=False,list_of_error
         n_exp=1
     else:
         n_exp=len(list_of_orders[0][:,0])#Number of exposures.
-        
+
 #===Then check that all orders indeed have n_exp exposures===
         for i in range(N):
             if len(list_of_orders[i][:,0]) != n_exp:

tayph/masking.py
@@ -61,7 +61,7 @@ def interpolate_over_NaNs(list_of_orders,cutoff=0.2):
     if N == 0:
         raise Exception('Runtime Error in interpolate_over_NaNs: List of orders is empty.')
 
-    N_nans_total = 0
+    # N_nans_total = 0
     N_nans_columns = 0
     N_nans_isolated = 0
     N_healed = 0
@@ -75,7 +75,7 @@ def interpolate_over_NaNs(list_of_orders,cutoff=0.2):
         N_pixels += nexp*npx
         list_of_masked_columns = []#This will contain the column numbers to mask completely at the end.
         if np.sum(np.isnan(order)) > 0:
-            N_nans_total+=np.sum(np.isnan(order))
+            # N_nans_total+=np.sum(np.isnan(order))
             #So this order contains NaNs.
             #First we loop over all columns to try to find columns where the number
             #of NaNs is greater than CUTOFF.
@@ -107,9 +107,10 @@ def interpolate_over_NaNs(list_of_orders,cutoff=0.2):
             for l in list_of_masked_columns:
                 order[:,l]+=np.nan#Set the ones that were erroneously healed back to nan.
         list_of_healed_orders.append(order)
-    print('------Number of bad pixels identified: %s (%s of total)' % (N_nans_total,N_nans_total/N_pixels))
-    print('------Number of pixels after masking columns: %s (%s of total)' % (N_nans_columns,N_nans_columns/N_pixels))
-    print('------Number of isolated pixels: %s (%s of total)' % (N_nans_isolated,N_nans_isolated/N_pixels))
+    print(f'------Total number of pixels in {N} orders: {N_pixels}')
+    print(f'------Number of NaNs in columns identified as bad (or previously masked): {N_nans_columns} ({np.round(N_nans_columns/N_pixels*100,2)}% of total)')
+    print(f'------Number of NaNs in isolated pixels: {N_nans_isolated} ({np.round(N_nans_isolated/N_pixels*100,2)}% of total)')
+    print(f'------Number of bad pixels identified: {N_nans_isolated+N_nans_columns} ({np.round((N_nans_isolated+N_nans_columns)/N_pixels*100,2)}% of total)')
     return(list_of_healed_orders)
 
 

tayph/run.py
@@ -93,7 +93,7 @@ def run_instance(p):
     import tayph.tellurics as telcor
     import tayph.masking as masking
     import tayph.models as models
-    import tayph.ccf as ccf
+    from tayph.ccf import xcor
     from tayph.vartests import typetest,notnegativetest,nantest,postest,typetest_array,dimtest
     # from lib import models
     # from lib import analysis
@@ -436,28 +436,38 @@ def run_instance(p):
                 os.makedirs(outpath)
             outpaths.append(outpath)
 
+
+
+#Perform the cross-correlation on the entire list of orders.
+    for i in range(len(list_of_wlts)):
+        templatename = templatelist[i]
+        wlt = list_of_wlts[i]
+        T = list_of_templates[i]
+        outpath = outpaths[i]
+        if do_xcor == True:
+            print(f'---Cross-correlating spectra with template {templatename}.')
+            t1=ut.start()
+            rv,ccf,ccf_e,Tsums=xcor(list_of_wls,list_of_orders_normalised,np.flipud(np.flipud(wlt)),T,drv,RVrange,list_of_errors=list_of_sigmas_normalised)
+            ut.end(t1)
+            print(f'------Writing CCFs to {str(outpath)}')
+            ut.writefits(outpath/'ccf.fits',ccf)
+            ut.writefits(outpath/'ccf_e.fits',ccf_e)
+            ut.writefits(outpath/'RV.fits',rv)
+            ut.writefits(outpath/'Tsum.fits',Tsums)
+        else:
+            print(f'---Reading CCFs with template {templatename} from {str(outpath)}.')
+            if os.path.isfile(outpath/'ccf.fits') == False:
+                raise FileNotFoundError(f'CCF output not located at {outpath}. Rerun with do_xcor=True to create these files?')
+        rv=fits.getdata(outpath/'rv.fits')
+        ccf = fits.getdata(outpath/'ccf.fits')
+        ccf_e = fits.getdata(outpath/'ccf_e.fits')
+        Tsums = fits.getdata(outpath/'Tsum.fits')
+
     sys.exit()
 
 
 
-#Perform the cross-correlation on the entire list of orders.
-    if do_xcor == True:
-        print('---Cross-correlating spectra')
-        rv,ccf,ccf_e,Tsums=ccf.xcor(list_of_wls,list_of_orders_normalised,np.flipud(np.flipud(wlt)),T,drv,RVrange,list_of_errors=list_of_sigmas_normalised)
-        print('------Writing CCFs to '+outpath)
-        ut.writefits(outpath+'ccf.fits',ccf)
-        ut.writefits(outpath+'ccf_e.fits',ccf_e)
-        ut.writefits(outpath+'RV.fits',rv)
-        ut.writefits(outpath+'Tsum.fits',Tsums)
-    else:
-        print('---Reading CCFs from '+outpath)
-        if os.path.isfile(outpath+'ccf.fits') == False:
-            print('------ERROR: CCF output not located at '+outpath+'. Set do_xcor to True to create these files?')
-            sys.exit()
-        rv=fits.getdata(outpath+'rv.fits')
-        ccf = fits.getdata(outpath+'ccf.fits')
-        ccf_e = fits.getdata(outpath+'ccf_e.fits')
-        Tsums = fits.getdata(outpath+'Tsum.fits')
+
 
     if plot_xcor == True and inject_model == False:
         print('---Plotting orders and XCOR')
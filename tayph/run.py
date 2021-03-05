a00c60787ee51f48599432346b9e3338e615503a
Jens Hoeijmakers <jens.hoeijmakers@space.unibe.ch>
2021-03-05 13:50:58 +0000

    Fix check_molecfit_gui

--- tayph/run.py
+++ tayph/run.py
@@ -1633,7 +1633,7 @@ def check_molecfit(dp,mode='HARPS',configfile=None):
     from pathlib import Path
     dp=ut.check_path(dp,exists=True)
     telpath = ut.check_path(Path(dp)/'telluric_transmission_spectra.pkl',exists=True)
-    list_of_wls,list_of_trans,list_of_fxc=read_telluric_transmission_from_file(telpath)
+    list_of_wls,list_of_trans,list_of_fxc=tel.read_telluric_transmission_from_file(telpath)
     to_do_manually = tel.check_fit_gui(list_of_wls,list_of_fxc,list_of_trans)
 
 

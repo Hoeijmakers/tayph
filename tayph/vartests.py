e9f70999a43fa5006431dac97bc19d30a6e808a1
Jens Hoeijmakers <jens.hoeijmakers@space.unibe.ch>
2021-06-07 19:02:18 +0100

    Solved bug in lentest.

--- tayph/vartests.py
+++ tayph/vartests.py
@@ -163,7 +163,8 @@ def lentest(var,length,varname='var'):
     typetest(varname,str)
     typetest(var,[list,tuple])
     if len(var) != length:
-        raise ValueError(f"Wrong length of {varname}:  len = {len(var)} but was required to be {ndim}.")
+        raise ValueError(f"Wrong length of {varname}:  len = {len(var)} but was required to be "
+        f"{length}.")
 
 
 

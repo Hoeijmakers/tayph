e8f924f0a56f290ce8f7f45c753a324288b8c6fa
N Borsato <n.borsato@astro.lu.se>
2021-06-29 20:04:40 +0200

    Trying to figure out fits list

--- tayph/operations.py
+++ tayph/operations.py
@@ -990,5 +990,5 @@ def fits_cleaner(uncleandirectory, cleandirectory, night, mode, cut_off = 0.1):
             shutil.copy(uncleandirectory + night + vis_files[i], cleandirectory + night + vis_files[i])
         elif mask[i] == False:
             print("Stopping Code")
-            quit()
+            #quit()
             #shutil.copy(uncleandirectory + night + vis_files[i], cleandirectory + night + vis_files[i])

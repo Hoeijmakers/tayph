f59c57a5ecac2f45ddeb8c9b2c35c9f76b2b022a
N Borsato <n.borsato@astro.lu.se>
2021-06-30 13:14:23 +0200

    Working out List mismatch

--- tayph/operations.py
+++ tayph/operations.py
@@ -976,6 +976,8 @@ def fits_cleaner(uncleandirectory, cleandirectory, night, mode, cut_off = 0.1, i
     sum_vals = sum_vals / np.median(sum_vals)#Normalise the summed values
     mask = sum_vals > cut_off #Creates a mask based on a specified cutoff
     print()
+    val = (path.isdir(cleandirectory)
+    print(val.type()))
     print(path.isdir(cleandirectory))
     if path.isdir(cleandirectory) == False:
         #This checks if the directory for the night in the cleaned spectra has been made

d7d4df978e902ce6b501af1305d846e726f265e1
Jens Hoeijmakers <jens.hoeijmakers@space.unibe.ch>
2021-11-09 15:43:17 +0100

    Add SPIROU explanation.

--- tayph/run.py
+++ tayph/run.py
@@ -1787,11 +1787,21 @@ save_figure=True,skysub=True):
             'the Tayph runfile of this dataset, do_berv_correction should be set to True and air '
             'in the config file of this dataset should be set to True.')]
 
+
         if mode in ['CARMENES-VIS']:
             explanation=[('[No explanation provided yet.]')]
 
         if mode in ['SPIROU']:
-            explanation=[('[No explanation provided yet.]')]
+            explanation=[('For SPIROU, no s1d spectra are provided, and the telluric correction is',
+            ' carried out by the pipeline on the e2ds spectra which are in the observatory ',
+            'rest-frame. The pipeline originally provided these in vaccuum wavelengths, but the ',
+            'read_spirou function should have converted these to air.'),'',('Therefore, you should',
+            ' see the following values for the the measured line centers:')('- The 2D spectra '
+            'correlated with PHOENIX should peak the systemic velocity minus the BERV correction '
+            f'(equal to {np.round(np.nanmean(berv),2)} km/s on average).'),('- The 2D spectra '
+            'correlated with the tellurics should peak at 0 km/s.'),'',('If this is correct, in '
+            'the Tayph runfile of this dataset, do_berv_correction should be set to True and air '
+            'in the config file of this dataset should be set to True.')]
 
 
 

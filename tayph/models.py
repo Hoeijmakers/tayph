b57ab9e1240c88165984511927cdd222edeb6f22
Jens Hoeijmakers <jens.hoeijmakers@space.unibe.ch>
2021-11-28 15:11:22 +0100

    Ignore empty lines in libfile.

--- tayph/models.py
+++ tayph/models.py
@@ -129,12 +129,9 @@ def get_model(name,library='models/library',root='models',is_binary=False):
 
     for i in range(0,n_lines):
         line=x[i].split()
-        try:
+        if len(line)>1:
             value=(line[1])
-        except:
-            print('Error reading content of library file. Entering debug mode.')
-            pdb.set_trace()
-        models[line[0]] = value
+            models[line[0]] = value
 
     try:
         modelpath=Path(models[name])
